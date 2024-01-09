45664ddff479e9c993218e81dbcc1cbc69005045
Taras Lashchiv <taraslasiv@gmail.com>
2024-01-09 15:46:23 +0200

    init

---
+++ main.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const fs = require('fs');
+
+const folder1 = '1';
+const folder2 = '2';
+const folder3 = '3';
+const folder4 = '4';
+const folder5 = '5';
+const file1 = '1.txt';
+const file2 = '2.txt';
+const file3 = '3.txt';
+const file4 = '4.txt';
+const file5 = '5.txt';
+
+fs.mkdir(folder1, err => {
+    if (err) {
+        console.error(`error ${err}`)
+    } else {
+        console.log(`${folder1} done`)
+    }
+});
+
+const filePath1 = `${folder1}/${file1}`;
+
+fs.writeFile(filePath1, '', err => {
+    if (err) {
+        console.error(`error file: ${err}`)
+    } else {
+        console.log(`file ${file1} created`)
+    }
+});
+
+fs.mkdir(folder2, err => {
+    if (err) {
+        console.error(`error ${err}`)
+    } else {
+        console.log(`${folder2} done`)
+    }
+});
+
+const filePath2 = `${folder2}/${file2}`;
+
+fs.writeFile(filePath2, '', err => {
+    if (err) {
+        console.error(`error file: ${err}`)
+    } else {
+        console.log(`file ${file2} created`)
+    }
+});
